05cadb4823061ac0a7d364ecc08ac126d8613032
MattiaSilvetri <mattiasilvestri27@gmail.com>
2024-10-26 17:45:38 +0200

    pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num > 0) {
+    let holder = "";
+    for (let i = 0; i < num; i++) {
+      holder += string;
+    }
+    return holder;
+  } else if (num === 0) {
+    return "";
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
