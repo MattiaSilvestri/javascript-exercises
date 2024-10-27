0b7389b3880dfc336004015c1cc4267311a37cff
MattiaSilvetri <mattiasilvestri27@gmail.com>
2024-10-27 17:00:56 +0100

    remove from array, pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  const arrSet = new Set(arr);
+  const argsSet = new Set(args);
+  arr = Array.from(arrSet);
+  args = Array.from(argsSet);
+  for (i of args) {
+    let index = arr.indexOf(i);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
