eb6cb31af7d47d251b29c459e557c81856973aaa
MattiaSilvetri <mattiasilvestri27@gmail.com>
2024-10-27 16:42:53 +0100

    reverse string pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let arrayed_string = string.split("");
+  let reversed_string = "";
+  for (let i = arrayed_string.length - 1; i >= 0; i--) {
+    reversed_string = reversed_string + arrayed_string[i];
+  }
+  return reversed_string;
 };
 
 // Do not edit below this line
