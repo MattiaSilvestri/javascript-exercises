37e055ddc5fedbe2b5b13e4eadce271d635c75a3
MattiaSilvetri <mattiasilvestri27@gmail.com>
2024-10-27 17:22:33 +0100

    sumall pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  function isPositveInteger(num) {
+    return Number.isInteger(num) && num > 0;
+  }
+  if (!isPositveInteger(min) || !isPositveInteger(max)) return "ERROR";
+  let sum = 0;
+  let actual_min = Math.min(min, max);
+  let actual_max = Math.max(min, max);
+  for (let i = actual_min; i <= actual_max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
